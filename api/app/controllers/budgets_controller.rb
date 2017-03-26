98aef33848897319108fcf71ce0b98393945dac7
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-03-26 16:59:53 +0800

    add budget: get all budget now call backend with controller

--- api/app/controllers/budgets_controller.rb
+++ api/app/controllers/budgets_controller.rb
@@ -2,9 +2,12 @@ class BudgetsController < ApplicationController
   before_action :set_account, only: [:show, :update, :destroy]
   before_action :authenticate_user!
 
-  # POST /accounts
   def create
     p params
+  end
+
+  def index
+    render json: [{month: '2017-03', amount: 1000}]
   end
 end
 
