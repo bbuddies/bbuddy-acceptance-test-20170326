fd6c418d1ccee615a076e1ef82db6f119c7b83f2
lizhenzhaofirst <707619335@qq.com>
2017-03-28 14:44:57 +0800

    correct total test

--- features/pages/total_page.rb
+++ features/pages/total_page.rb
@@ -7,10 +7,13 @@ require_relative 'page_base'
    def go_to_total_budgets
      touch 'Total'
    end
+   def go_to_cal_total_budgets
+     touch 'SumBudget'
+   end
    def sum_total_budgets(date1, date2)
      enter_text 'start_date', date1
      enter_text 'end_date', date2
-     touch 'SumBudget'
+     touch 'Sum'
    end
    def assert_total_budgets(budget)
      wait_for_element_does_not_exist 'Sum'
--- features/step_definitions/total_steps.rb
+++ features/step_definitions/total_steps.rb
@@ -2,10 +2,11 @@
 
   假如(/^存在如下预算$/) do |total|
     TotalPage.open.go_to_total_budgets
+    TotalPage.open.go_to_cal_total_budgets
   end
 
   当(/^查询开始日期"([^"]*)"，结束时间"([^"]*)"的预算$/) do |date1, date2|
-    TotalPage.open.sum_total_udgets(date1, date2)
+    TotalPage.open.sum_total_budgets(date1, date2)
   end
 
   那么(/^预算为(\d+)$/) do |totalBudgets|
